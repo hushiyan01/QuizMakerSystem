6f9b9d97034aca785a3d42b37b9f693d7064174c
胡适言 <hushiyan@hushiyandeMacBook-Pro.local>
2023-06-22 16:10:25 +0200

    1

--- src/main/java/com/isom/a4_team_5/gui/Test.java
+++ src/main/java/com/isom/a4_team_5/gui/Test.java
@@ -1,5 +1,12 @@
-package com.isom.a4_team_5.gui;/**
+package com.isom.a4_team_5.gui;
+
+import javax.swing.*;
+
+/**
  * @author hushiyan
  * @date 3/7/23 7:36 AM
- */public class Test {
+ */
+public class Test {
+    private JButton button1;
+    private JButton button2;
 }
